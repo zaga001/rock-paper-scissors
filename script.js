a4badf9f3e2f744956c277bf3a406e6ce5e8dc73
zaga <163298261+zaga001@users.noreply.github.com>
2024-05-02 15:26:35 +0700

    Add code for a round of rock paper scissors

--- script.js
+++ script.js
@@ -20,7 +20,17 @@ function getHumanChoice() {
 let humanScore = 0;
 let computerScore = 0;
 function playRound(humanChoice, computerChoice) {
-
+    if (humanChoice === computerChoice) {
+        console.log("It's a tie!");
+    } else if ((humanChoice === 'rock' && computerChoice === 'scissors') || (humanChoice === 'scissors' && computerChoice === 'papers') || (humanChoice === 'papers' && computerChoice === 'rock')) {
+        console.log(`You win! ${humanChoice.charAt(0).toUpperCase()+ humanChoice.slice(1)} beats ${computerChoice}.`);
+        humanScore += 1;
+    } else if (humanChoice === 'error') {
+        console.log('You have entered an invalid choice. Try again.');
+    } else {
+        console.log(`You lose! ${computerChoice.charAt(0).toUpperCase()+ computerChoice.slice(1)} beats ${humanChoice}.`);
+        computerScore += 1;
+    }
 }
 const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice();
